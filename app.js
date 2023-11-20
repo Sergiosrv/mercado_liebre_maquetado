7edcc0a1a67d477bebce383a33c6accb3e0f6d05
SergioSRV <sergiosrv@gmail.com>
2023-11-20 16:57:59 -0300

    subida 003_DisenioAdaptativo v002 arreglado image

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+
 const PORT=3030;
+
 const app = express()
+
 app.use(express.static(path.join(__dirname,'public')))
+
 app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views','home.html')))
 app.listen(PORT,() => console.log(`server running http://localhost:${PORT}`));
