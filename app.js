5b356f3708a21c25d9daf348c88f2d6bee2f1200
SergioSRV <sergiosrv@gmail.com>
2023-11-20 22:38:34 -0300

    Subida de 005_Formularios v1

--- app.js
+++ app.js
@@ -8,4 +8,7 @@ const app = express()
 app.use(express.static(path.join(__dirname,'public')))
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views','home.html')))
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname, 'views','register.html')))
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname, 'views','login.html')))
+
 app.listen(PORT,() => console.log(`server running http://localhost:${PORT}`));
